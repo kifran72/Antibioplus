d8528635f8cf5a61e86c79e5588212f136bbd86e
Yann LM <yannlm2010@hotmail.fr>
2017-05-03 19:17:05 +0200

    Ajout des méthodes nécessaire à la création d'une équipe

--- modele/equipe.php
+++ modele/equipe.php
@@ -1,8 +1,7 @@
-<?php 
+<?php
 require_once '../../Connexion/Config.php';
 
     class Equipe {
-        
         function getEquipe(){
             $db = new PDO("mysql:host=" . Config::SERVERNAME . ";dbname=" . Config::DBNAME, Config::USER, Config::PASSWORD);
             $req = $db->prepare("SELECT * FROM equipe");
@@ -11,7 +10,6 @@ require_once '../../Connexion/Config.php';
 
             return $resultat;
         }
-
         function getEquipeByName($name){
             $db = new PDO("mysql:host=" . Config::SERVERNAME . ";dbname=" . Config::DBNAME, Config::USER, Config::PASSWORD);
             $req = $db->prepare("SELECT * FROM equipe WHERE Nom_Equipe LIKE '". $name ."%'");
@@ -21,7 +19,22 @@ require_once '../../Connexion/Config.php';
             return $resultat;
         }
         function deleteEquipeByName($id){
-            return $id;
+            $db = new PDO("mysql:host=" . Config::SERVERNAME . ";dbname=" . Config::DBNAME, Config::USER, Config::PASSWORD);
+            $req = $db->prepare("DELETE FROM equipe WHERE ID_Equipe='". $id ."'");
+            $req->execute();
+
+            return true;
+        }
+        function addEquipe($name){
+            $db = new PDO("mysql:host=" . Config::SERVERNAME . ";dbname=" . Config::DBNAME, Config::USER, Config::PASSWORD);
+            $req = $db->prepare("INSERT INTO equipe(Nom_Equipe, Actif, En_Etude) VALUES('".$name."', '1', '0')");
+
+            if($req->execute()){
+                $id = $db->lastInsertId();
+                return $id;
+            } else {
+                return false;
+            }
         }
     }
 ?>
--- modele/utilisateur.php
+++ modele/utilisateur.php
@@ -14,7 +14,19 @@ require_once '../../Connexion/Config.php';
 
         function fillInputUtilisateur($list_p){
             foreach($list_p as $utilisateur){
-                echo "<option>".$utilisateur['Nom_Personne']."</option>";
+                echo "<option value='".$utilisateur['ID_Personne']."'>".$utilisateur['Nom_Personne']."</option>";
+            }
+        }
+
+        function associerUtilisateurEquipe($userId, $equipeId){
+            $db = new PDO("mysql:host=" . Config::SERVERNAME . ";dbname=" . Config::DBNAME, Config::USER, Config::PASSWORD);
+            $req = $db->prepare("UPDATE personne SET ID_Equipe = '". $equipeId ."' WHERE ID_Personne='". $userId ."'");
+
+            if($req->execute()){
+                return true;
+            }
+            else {
+                return false;
             }
         }
 
