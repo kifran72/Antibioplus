f8afe6edd9693a1a94d05c8f7e32745ddf38ac1d
kifran72 charles r <cr91700kifran@gmail.com>
2017-05-03 12:51:14 +0200

    final push

--- Connexion/Config.php
+++ Connexion/Config.php
@@ -4,8 +4,8 @@
 class Config {
     const SERVERNAME="localhost";
     const DBNAME="ProjetDev2017";
-    const USER="root";
-    const PASSWORD="";
+    const USER="ProjetDev2017";
+    const PASSWORD="epsi1234";
 }
 
 
